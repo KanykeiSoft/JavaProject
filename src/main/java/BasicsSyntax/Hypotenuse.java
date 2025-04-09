c1c49cea661f8e5122ec12c96ec9d141fd3c2d35
Kanykei <kana123k@gmail.com>
2025-04-09 18:01:12 -0400

    overloading

---
+++ src/main/java/BasicsSyntax/Hypotenuse.java
@@ -0,0 +1,15 @@
+package BasicsSyntax;
+
+import java.util.Scanner;
+
+public class Hypotenuse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        if(a <= 100 && b <=100){
+            double c = Math.sqrt(a * a + b * b);
+            System.out.println("Гипотенуза: " + c);
+        }
+    }
+}
