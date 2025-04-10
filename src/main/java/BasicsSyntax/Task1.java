0a8aedf096b2883a796bb160468f70e776c7bb01
Kanykei <kana123k@gmail.com>
2025-04-09 21:39:06 -0400

    overloading

---
+++ src/main/java/BasicsSyntax/Task1.java
@@ -0,0 +1,23 @@
+package BasicsSyntax;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int sum =k % n;
+        System.out.print(sum);
+    }
+        }
+
+
+
+
+
+
+
+
+
+
