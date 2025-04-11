30d8252caff1a756aae7a6892528a7f7bcfeb686
Kanykei <kana123k@gmail.com>
2025-04-11 01:00:39 -0400

    overloading

---
+++ src/main/java/Oop/Person.java
@@ -0,0 +1,28 @@
+package Oop;
+
+public class Person {
+    private String fullName;
+    private int age;
+    private String gender;
+
+
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+}
