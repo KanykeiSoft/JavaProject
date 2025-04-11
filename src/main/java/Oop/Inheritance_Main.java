50480f1ad3408a08e480015d38a6090594d6bdcb
Kanykei <kana123k@gmail.com>
2025-04-11 09:58:34 -0400

    overloading

--- src/main/java/Oop/Inheritance_Main.java
+++ src/main/java/Oop/Inheritance_Main.java
@@ -2,6 +2,7 @@ package Oop;
 
 public class Inheritance_Main {
     public static void main(String[] args) {
+        Student koka = new Student("Koka", 27, "m", 111, "lo",98);
 
     }
 }
--- src/main/java/Oop/Student.java
+++ src/main/java/Oop/Student.java
@@ -1,4 +1,31 @@
 package Oop;
 
 public class Student extends Person{
+    private long id;
+    private String degreeProgram;
+    private double cqpa;
+
+
+    public Student(){
+        this.id = 2435;
+        this.degreeProgram = "Not-assigned";
+        this.cqpa = 0.0;
+    }
+    public Student(
+
+            String name,
+            int age,
+            String gender,
+            long id,
+            String degreeProgram,
+            double cqpa
+            ){
+        setFullName(name);
+        setGender(gender);
+        setAge(age);
+        this.id =id;
+        this.degreeProgram = degreeProgram;
+        this.cqpa =cqpa;
+
+    }
 }
