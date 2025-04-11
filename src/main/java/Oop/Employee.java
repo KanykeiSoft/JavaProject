e32231963fc41b95920908b4af5db816c2c91051
Kanykei <kana123k@gmail.com>
2025-04-11 01:12:38 -0400

    overloading

--- src/main/java/Oop/Employee.java
+++ src/main/java/Oop/Employee.java
@@ -1,4 +1,34 @@
 package Oop;
 
 public class Employee extends Person{
+    private String department;
+    private String jobTitle;
+    private double salary;
+    private String companyName;
+
+    public Employee(){
+        this.department = "not-assigned";
+        this.jobTitle = "unknown";
+        this.salary = 0;
+        this.companyName = "unknown";
+    }
+
+    public Employee(
+         String name,
+         int age,
+         String gender,
+         String department,
+         String jobTitle,
+         double salary,
+         String companyName
+    ){
+        setFullName(name);
+        setAge(age);
+        setGender(gender);
+        this.department = department;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+        this.companyName = companyName;
+
+    }
 }
---
+++ src/main/java/Oop/Inheritance_Main.java
@@ -0,0 +1,7 @@
+package Oop;
+
+public class Inheritance_Main {
+    public static void main(String[] args) {
+
+    }
+}
