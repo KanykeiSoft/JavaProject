b4b71a0f5235dd14a0fce55d24ff7b8d719eb15c
Kanykei <kana123k@gmail.com>
2025-04-30 23:08:50 -0400

    Stack

--- src/main/java/Colection/HashMap_Main.java
+++ src/main/java/Colection/HashMap_Main.java
@@ -1,9 +1,32 @@
 package Colection;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class HashMap_Main {
+
     public static void main(String[] args) {
+        System.out.println("{}{} -----> " + areBalanced("{}{}"));
+        System.out.println("{}}{}{} -----> " + areBalanced("{}}{}{}"));
+        System.out.println("{{}}{} -----> " + areBalanced("{{}}{}"));
 
     }
+
+    public static boolean areBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char charactor : str.toCharArray()) {
+            if (charactor == '{') {
+                stack.push(charactor);
+            }
+            else if (charactor == '}') {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+
+            }
+        }
+        return stack.isEmpty();
+    }
 }
+
