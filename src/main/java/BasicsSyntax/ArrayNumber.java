9431c8fa0623c192a12c9bba13469b32f68c3617
Kanykei <kana123k@gmail.com>
2025-05-14 00:14:14 -0400

    array

---
+++ src/main/java/BasicsSyntax/ArrayNumber.java
@@ -0,0 +1,18 @@
+package BasicsSyntax;
+
+import java.util.Scanner;
+
+public class ArrayNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = sc.nextInt();
+        }
+        for(int i = 0; i <n; i+=2){
+            System.out.println(array[i] + " ");
+        }
+
+    }
+}
