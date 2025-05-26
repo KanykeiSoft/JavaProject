c643dd28710881909c455b0a55b19db41e4b3894
Kanykei <kana123k@gmail.com>
2025-05-25 23:45:38 -0400

    Test

---
+++ src/main/java/LeetCode/ListNode.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class ListNode {
+    
+    public static void main(String[] args) {
+
+        // Example of binary search on a sorted array
+        int[] nums = {1, 3, 5, 7, 9, 11, 13};
+        int target = 7;
+        int left = 0, right = nums.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                break;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("Index of target: " + result);
+
+
+
+
+
+      //write me some code where use web api
+
+
+        }
+}
--- src/main/java/Streams.java
+++ src/main/java/Streams.java
@@ -1,18 +1,32 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Streams {
     public static void main(String[] args) throws IOException {
-        InputStream is = new FileInputStream("src/main/java/Vehicle/input.txt");
-        byte[] allByte = is.readAllBytes();
-        String result = new String(allByte);
-        System.out.println(result);
+//        InputStream is = new FileInputStream("src/main/java/Vehicle/input.txt");
+//        byte[] allByte = is.readAllBytes();
+//
+//        for(byte b: allByte){
+//            System.out.printf("%d ", b);
+//        }
+//        System.out.println();
+//
+//
+//        Reader reader = new FileReader("src/main/java/Vehicle/input.txt");
+//        char[] inpit = new char[256];
+//       int n =  reader.read(inpit);
+//        System.out.println(new String(inpit, 0, n));
 
-        Reader reader = new FileReader("src/main/java/Vehicle/input.txt");
-        char[] inpit = new char[256];
-       int n =  reader.read(inpit);
-        System.out.println(new String(inpit, 0, n));
 
+        Writer writer = new OutputStreamWriter(
+                new FileOutputStream("src/main/java/Vehicle/input.txt"),
+                StandardCharsets.UTF_8 // <-- Вот тут задаем кодировку
+        );
+        writer.write("Aidar is my love");
+        writer.flush();
+        writer.close(); // Тоже закрываем
+    }
 
 
     }
-}
+
