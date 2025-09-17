a11d7a5e544de4da13ab679e47d8e42219c96904
Kanykei <kana123k@gmail.com>
2025-09-16 22:51:03 -0400

    Palindrome

---
+++ src/main/java/Arrays/Fibonacce.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Fibonacce {
+    public static int fib(int n) {
+        if (n == 0) return 0;   // базовый случай 1
+        if (n == 1) return 1;   // базовый случай 2
+        return fib(n - 1) + fib(n - 2); // рекурсивный вызов
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // ← здесь можно задать число
+        int result = fib(n);
+        System.out.println("fib(" + n + ") = " + result);
+    }
+
+
+}
