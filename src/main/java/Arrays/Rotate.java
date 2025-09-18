6f13c9b0fd7bfbd8760d2511425b7bbcbad2b010
Kanykei <kana123k@gmail.com>
2025-09-18 10:57:28 -0400

    sort

---
+++ src/main/java/Arrays/Rotate.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Rotate {
+    public void rotateNum(int[] nums, int k) {
+        int n = nums.length;
+        if(n==0) return;
+        k%=n;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        int k = 3;
+        Rotate r  =new Rotate();
+        r.rotateNum(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+}
