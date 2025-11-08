26b6a2dc8fac5e99dccdd3eefce0db2726897397
Kanykei <kana123k@gmail.com>
2025-11-07 22:52:52 -0500

    recursion

--- src/main/java/Bank_app/findMin.java
+++ src/main/java/Bank_app/findMin.java
@@ -1,7 +1,36 @@
 package Bank_app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class findMin {
+
+    public List<String> generate(int n){
+        List<String> res = new ArrayList<>();
+        backtrack(res, 0 ,0 ,"",n);
+        return res;
+    }
+
+     public void backtrack(List<String> res, int left, int right, String s, int n){
+         if(s.length()==2 *n){
+             res.add(s);
+             return;
+
+         }
+         if (left < n) {
+             backtrack(res, left + 1, right, s + "(", n);
+         }
+
+         // Если можно добавить закрывающую скобку
+         if (right < left) {
+             backtrack(res, left, right + 1, s + ")", n);
+         }
+
+     }
     public static void main(String[] args) {
-        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+    findMin find = new findMin();
+    int n = 3;
+        List<String> result = find.generate(n);
+        System.out.println(result);
     }
 }
